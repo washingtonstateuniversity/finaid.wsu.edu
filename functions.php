eadd7cc4b73f4ac17838aa6e5bd795f4d3da69f2
Phil Cable <pcable@wsu.edu>
2016-08-23 08:42:08 -0700

    Register menu and introduce markup for a footer menu

--- functions.php
+++ functions.php
@@ -1,6 +1,45 @@
 <?php
+
+class WSU_Student_Financial_Services_Theme {
+	/**
+	 * @var WSU_Student_Financial_Services_Theme
+	 */
+	private static $instance;
+	/**
+	 * Maintain and return the one instance and initiate hooks when
+	 * called the first time.
+	 *
+	 * @return \WSU_Student_Financial_Services_Theme
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new WSU_Student_Financial_Services_Theme;
+			self::$instance->setup_hooks();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Setup hooks to include.
+	 */
+	public function setup_hooks() {
+		add_action( 'init', array( $this, 'register_menu' ), 10 );
+	}
+
+	/**
+	 * Register the additional menu used in the theme footer.
+	 */
+	public function register_menu() {
+		register_nav_menu( 'footer', 'Footer' );
+	}
+
+}
+add_action( 'after_setup_theme', 'WSU_Student_Financial_Services_Theme' );
 /**
- * Custom functionality required by your child theme can go here. Use this
- * to override any defaults provided by the Spine parent theme through
- * the provided actions and filters.
+ * Start things up.
+ *
+ * @return \WSU_Student_Financial_Services_Theme
  */
+function WSU_Student_Financial_Services_Theme() {
+	return WSU_Student_Financial_Services_Theme::get_instance();
+}
---
+++ parts/footers.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Include a footer navigation area inside the `main` element on all pages.
+ */
+$footer_menu_args = array(
+	'theme_location' => 'footer',
+	'menu' => 'footer',
+	'container' => false,
+	'container_class' => false,
+	'fallback_cb' => false,
+	'menu_class' => 'site-footer-menu',
+	'menu_id' => false,
+	'depth' => 2,
+);
+?>
+<footer class="side-right row gutter padded-ends pad-ends site-footer">
+	<div class="column one">
+		<?php wp_nav_menu( $footer_menu_args ); ?>
+	</div>
+	<div class="column two">
+		<p>Connect</p>
+
+	</div>
+</footer>
