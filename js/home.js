ab9dfd2049dc5fb79c8be90a57540a8caf991f22
Phil Cable <pcable@wsu.edu>
2016-09-02 11:25:46 -0700

    Add documentation

--- js/home.js
+++ js/home.js
@@ -2,10 +2,13 @@
 
 	'use strict';
 
+	// Add Previous and Next controls for navigating the output of the Content Syndicate shortcode.
 	$('.home .wsuwp-content-syndicate-list').before('<a href="#" class="event-control prev">Previous</a>').after('<a href="#" class="event-control next">Next</a>');
 
+	// Hide the Previous control - we only want to show it when it's needed.
 	$('.event-control.prev').hide();
 
+	// Adjust the left offset of the Content Syndicate list accordingly when the controls are clicked.
 	$('.wsuwp-content-syndicate-wrapper').on('click', '.event-control', function (e) {
 		e.preventDefault();
 
@@ -15,13 +18,14 @@
 			right_max = -(event.length * event.outerWidth() - list.width());
 
 		if ($(this).hasClass('prev')){
-			$('.wsuwp-content-syndicate-list').css('left', '+=' + event.outerWidth() + 'px');
+			list.css('left', '+=' + event.outerWidth() + 'px');
 		} else if ($(this).hasClass('next')){
-			$('.wsuwp-content-syndicate-list').css('left', '-=' + event.outerWidth() + 'px');
+			list.css('left', '-=' + event.outerWidth() + 'px');
 		}
 
 		position = parseInt(list.css('left'));
 
+		// Show the controls when list items are available to scroll to, and hide them otherwise.
 		if (0 <= position) {
 			$('.event-control.prev').hide();
 		} else {
