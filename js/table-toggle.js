d898ce4191f0dd6d6bc13c69936e3a26375b1a40
Phil Cable <pcable@wsu.edu>
2016-09-06 09:04:14 -0700

    Cache selectors, fix to work with tables using a responsive mode

--- js/table-toggle.js
+++ js/table-toggle.js
@@ -3,29 +3,32 @@
 
 	'use strict';
 
+	var $list = $('.dropdown-content'),
+		$filter = $('.dropdown');
+
 	// For all tables grouped together in a div, hide all but the first.
-	$('div table:not(:first-child)').hide();
+	$('div .tablepress-scroll-wrapper:not(:first-child)').hide();
 
 	// When a link in the .dropdown-content element is clicked, show
 	// the table with the corresponding id and hide all its siblings.
-	$('.dropdown-content').on('click', 'a', function (e) {
+	$list.on('click', 'a', function (e) {
 		e.preventDefault();
 
-		var table = '#tablepress-' + this.hash.replace('#', '');
+		var table = '#tablepress-' + this.hash.replace('#', '') + '-scroll-wrapper';
 
 		$(table).show().siblings().hide();
 
 		// Hide the list of links.
-		$('.dropdown-content').hide();
+		$list.hide();
 	});
 
 	// Show the list of links when the cursor is over the .dropdown element.
-	$('.dropdown').on('mouseover', function () {
+	$filter.on('mouseover', function () {
 		$(this).find('.dropdown-content').show();
 	});
 
 	// Hide the list of links when the cursor is not over the .dropdown element.
-	$('.dropdown').on('mouseout', function () {
+	$filter.on('mouseout', function () {
 		$(this).find('.dropdown-content').hide();
 	});
 }(jQuery));
