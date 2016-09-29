56ae3aebdaf2b56f61fc017d2d9da356120efdae
Phil Cable <pcable@wsu.edu>
2016-09-29 15:57:20 -0700

    Use the Scholarships plugin 'search_page_url` option value as the "Back..." link href

--- parts/single-layout-scholarship.php
+++ parts/single-layout-scholarship.php
@@ -1,10 +1,18 @@
+<?php
+$settings = get_option( 'scholarships_settings' );
+if ( $settings['search_page_url'] ) {
+	$search_page_url = get_permalink( $settings['search_page_url'] );
+}
+?>
 <section class="row single gutter pad-ends">
 
 	<div class="column one">
 
+		<?php if ( $search_page_url ) { ?>
 		<p>
-			<a href="#">« Scholarship Search Results</a>
+			<a href="<?php echo esc_url( $search_page_url ); ?>">« Scholarship Search Results</a>
 		</p>
+		<?php } ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
