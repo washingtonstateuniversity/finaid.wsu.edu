e2f81a91efd6bad53a8d3519b6988f7129717489
Phil Cable <pcable@wsu.edu>
2017-12-11 11:33:04 -0800

    Bump version 0.1.0

--- includes/class-wsu-student-financial-services-theme.php
+++ includes/class-wsu-student-financial-services-theme.php
@@ -8,7 +8,7 @@ class WSU_Student_Financial_Services_Theme {
 	 *
 	 * @var string String used for busting cache on scripts.
 	 */
-	public $script_version = '0.0.15';
+	public $script_version = '0.1.0';
 
 	/**
 	 * @since 0.0.1
